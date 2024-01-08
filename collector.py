5f96a3446283eea95c101de403d7d23b2a9fe9c4
Won Seo <swhors@gmail.com>
2024-01-08 10:37:26 +0900

    Update collector.py

--- collector.py
+++ collector.py
@@ -3,11 +3,12 @@ import time
 import os, sys
 from flask import Flask
 import socket
+import daemon
 
 app = Flask(__name__)
 host_name = socket.gethostname()
-resource_name = "eimmo-batch-vms" # 리소스 그룹 이름
-service_name = "eimmo-batch-vms-worker3" # 서비스 이름 (호스트 이름)
+resource_name = "" # edit resource group name
+service_name = "" # edif : instance name
 
 
 def get_machine_cpu():
@@ -19,6 +20,117 @@ def get_machine_cpu():
 @app.route('/about')
 def about():
     return "metric_collector for vm\n", 200
+
+
+@app.route('/metric/docker')
+def view_docker_ps_cnt():
+    import subprocess
+    keys = {"azure_vm_docker_count": {"instance": "", "metric": "docker_count", "resource": resource_name, "service": service_name}}
+    metric_num = subprocess.check_output("/usr/bin/ps -adef|/usr/bin/grep 'docker run'|/usr/bin/grep -Ewv 'ps|grep'|/usr/bin/wc -l", shell=True, text=True)
+    registry = CollectorRegistry()
+
+    label = keys["azure_vm_docker_count"]
+    label["instance"] = host_name
+    gauge = Gauge("azure_vm_docker_count", "azure_vm_docker_count", label.keys(), registry=registry)
+    label_values = label.values()
+    gauge.labels(*label_values).set(metric_num)
+    metric = generate_latest(registry=registry)
+
+    return metric.decode('utf-8'), 200
+
+
+@app.route('/metric/docker-abnormal')
+def view_docker_ps_abnormal_cnt():
+    import subprocess
+    keys = {"azure_vm_docker_abnormal_count": {"instance": "", "metric": "docker_abnormal_count", "resource": resource_name, "service": service_name}}
+    cmd = "/usr/bin/docker ps --filter=status=exited --filter=status=created |/usr/bin/wc -l"
+    metric_num = subprocess.check_output(cmd, shell=True, text=True)
+    registry = CollectorRegistry()
+    label = keys["azure_vm_docker_abnormal_count"]
+    label["instance"] = host_name
+    gauge = Gauge("azure_vm_docker_abnormal_count", "azure_vm_docker_abnormal_count", label.keys(), registry=registry)
+    label_values = label.values()
+    gauge.labels(*label_values).set(metric_num)
+    metric = generate_latest(registry=registry)
+
+    return metric.decode('utf-8'), 200
+
+
+@app.route('/metric/process')
+def view_process_cnt():
+    import subprocess
+    keys = {"azure_vm_process_count": {"instance": "", "metric": "process_count", "resource": resource_name, "service": service_name}}
+    metric_num = subprocess.check_output("/usr/bin/ps -adef|/usr/bin/grep -Ewv 'ps |grep' |/usr/bin/wc -l", shell=True, text=True)
+    registry = CollectorRegistry()
+
+    label = keys["azure_vm_process_count"]
+    label["instance"] = host_name
+    gauge = Gauge("azure_vm_process_count", "azure_vm_process_count", label.keys(), registry=registry)
+    label_values = label.values()
+    gauge.labels(*label_values).set(metric_num)
+    metric = generate_latest(registry=registry)
+
+    return metric.decode('utf-8'), 200
+
+
+def get_folder_count(pid: str) -> int:
+    path = f"/proc/{pid}/fd"
+    folder_count = sum([len(folder) for r, d, folder in os.walk(path)])
+    return folder_count
+
+
+def get_fd_cnt_by_ps():
+    import subprocess
+    cmd = "/usr/bin/ps -adef|/usr/bin/grep -Ewv 'grep|ps '|/usr/bin/awk '{print $2}'"
+    pids_str = subprocess.check_output(cmd, shell=True, text=True)
+    pids = pids_str.split("\n")
+    folder_num = 0
+    for pid in pids:
+        if len(pid) > 0 and pid.isdecimal():
+            folder_num += get_folder_count(pid)
+    
+    return folder_num
+
+
+def get_fd_cnt_by_lsof():
+    import subprocess
+    return subprocess.check_output("/usr/bin/lsof|/usr/bin/grep -Ewv 'grep|lsof'|/usr/bin/wc -l", shell=True, text=True)
+
+
+def view_fd_internal(cmd_type: int):
+    keys = {"azure_vm_opened_fd_count": {"instance": "", "metric": "opened_fd_count", "resource": resource_name, "service": service_name, "cmd_type": cmd_type}}
+
+    if cmd_type == 1:
+        metric_num = get_fd_cnt_by_lsof()
+    else:
+        metric_num = get_fd_cnt_by_ps()
+
+    registry = CollectorRegistry()
+
+    label = keys["azure_vm_opened_fd_count"]
+    label["instance"] = host_name
+    gauge = Gauge("azure_vm_opened_fd_count", "azure_vm_opened_fd_count", label.keys(), registry=registry)
+    label_values = label.values()
+    gauge.labels(*label_values).set(metric_num)
+    metric = generate_latest(registry=registry)
+
+    return metric.decode('utf-8'), 200
+
+
+@app.route('/metric/fd/<cmd_type>')
+def view_fd(cmd_type: int):
+    try:
+        return view_fd_internal(cmd_type)
+    except Exception as e:
+        return str(e), 500
+
+
+@app.route('/metric/fd')
+def view_fd_default():
+    try:
+        return view_fd_internal(2)
+    except Exception as e:
+        return str(e), 500
 
 
 @app.route('/metric/cpu')
@@ -123,73 +235,6 @@ def main():
     app.run(host="0.0.0.0",port=5001)
 
 
-def run_as_fork():
-    pid = os.fork()
-    if pid > 0:
-        exit(0)
-    else:
-        os.chdir("/")
-        os.setsid()
-        os.umask(0)
-
-        pid = os.fork()
-        if pid > 0:
-            exit(0)
-        else:
-            sys.stdin.flush()
-            sys.stdout.flush()
-
-            si = open(os.devnull, 'r')
-            so = open(os.devnull, 'a+')
-            se = open(os.devnull, 'a+')
-
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
-
-            with open("/var/run/collector.pid","w") as write:
-                write.write(str(os.getpid()))
-
-            main()
-
-        os.umask(0)
-
-        pid = os.fork()
-        if pid > 0:
-            exit(0)
-        else:
-            sys.stdin.flush()
-            sys.stdout.flush()
-
-            si = open(os.devnull, 'r')
-            so = open(os.devnull, 'a+')
-            se = open(os.devnull, 'a+')
-
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
-
-            with open("/var/run/collector.pid","w") as write:
-                write.write(str(os.getpid()))
-
-            main()
-
-
-def print_usage():
-    print("python collector.py [options]")
-    print("------------------------------")
-    print("options:")
-    print("  -d : run as forked daemon")
-    print("  -p : run as self process")
-
-
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if len(args) == 0:
-        run_as_fork()
-    if args[0] == "-d":
-        run_as_fork()
-    elif args[0] == "-p":
+    with daemon.DaemonContext():
         main()
-    else:
-        print_usage()
